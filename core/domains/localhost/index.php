7a5857c4d1c093e100a48e07eb1d9cfaa5c83179
Danila <dp23092002@gmail.com>
2020-10-29 20:25:50 +0700

    Main: commit 2

---
+++ core/domains/localhost/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <?php
+        $a = 10;
+        $b = 1000;
+        echo $a;
+        echo "<hr>";
+        echo $b;
+      ?>
+  </body>
+</html>
